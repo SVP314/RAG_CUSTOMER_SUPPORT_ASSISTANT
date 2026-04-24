fe17049e6e8e85a7aff7da438528852f58ddeed7
SVP314 <pvswathi2006@gmail.com>
2026-04-24 20:04:04 +0530

    Create config.py

---
+++ App/config.py
@@ -0,0 +1,12 @@
+COLLECTION_NAME = "support_kb"
+
+PERSIST_DIR = "./chroma_store"
+
+PDF_PATH = "./data/knowledge_base.pdf"
+
+CHUNK_SIZE = 800
+CHUNK_OVERLAP = 120
+
+TOP_K = 4
+
+CONFIDENCE_THRESHOLD = 0.5
